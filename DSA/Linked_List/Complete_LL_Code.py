5a140ec2cf7294e3fa685f081a3369f8698ed3d4
begudem <srinath.begudem@nethermind.io>
2025-08-22 18:24:00 -0400

    added intro LL

--- DSA/Linked_List/Complete_LL_Code.py
+++ DSA/Linked_List/Complete_LL_Code.py
@@ -11,7 +11,7 @@ class LinkedList:
     def __init__(self, iterable=None):
         self.head = None
         self.tail = None
-        self.size = 0
+        self._size = 0
         if iterable:
             for val in iterable:
                 self.append(val)
@@ -34,8 +34,8 @@ class LinkedList:
         else:
             self.tail.next = new_node
             self.tail = new_node
-        #increase the size of ll to reflect the true len of ll
-        self.size += 1
+        #increase the _size of ll to reflect the true len of ll
+        self._size += 1
     
     def prepend(self,val):
         """
@@ -50,7 +50,88 @@ class LinkedList:
         # edge case if the LL is empty 
         if self.tail is None:
             self.tail = new_node # both head and tail should point to new_node
-        self.size += 1 # increase the size of ll
+        self._size += 1 # increase the _size of ll
+    
+    #-------internal--------
+    def _node_at(self,index):
+        """
+        Return the node at index (0 based indexing)
+        """
+        cur = self.head
+        for _ in index:
+            cur = cur.next
+        return cur 
+    
+    def insert_at(self, index, val):
+        """
+        To insert a val at given index, time is 0(n) coz of travsersal
+        """
+        #edge cases
+        if index < 0 or index > self._size:
+            raise IndexError("Index out of bounds")
+        if index == 0:
+            self.prepend(val)
+        if index == self._size:
+            self.append(val)
+
+        #traverse till index using _node_at function 
+        prev = self._node_at(index-1)
+        #create a new node and directly link it to the prev.nxt
+        new_node = ListNode(val,prev.next)
+        #now update prev.next to point towards the new node
+        prev.next = new_node
+        #increase the _size
+        self._size += 1
+    
+    def pop_front(self):
+        """
+        Remove the first element in LL and return it, time is o(1)
+        """
+        #edge case 
+        if not self.head :
+            raise IndexError("Linked List is empty")
+        node = self.head
+        self.head = node.next
+        # if we only have one node and we popped it then we do
+        if self.head is None:
+            self.tail = None
+        self._size -= 1
+        return node.val
+    
+    def pop_end(self):
+        """
+        The important point is that the singly linked list 
+        thou it has the tail we cannot move backwards, 
+        so to remove last element we need to move one step back and 
+        set last before node pointing towards None, as i mention we 
+        cannot move back therefore we need to travser from start 
+        Time = O(1) and we usually have a single fucntion called 
+        remove_at for poping the end element, but here for pratice 
+        we are just calling remove_at function for _size - 1 for pop_end
+        """
+        return self.remove_at(self._size-1)
+    
+    def remove_at(self, index):
+        """
+        Removing the node at given index, time o(n) walk
+        """
+        if index < 0 or index >= self._size:
+            raise IndexError("index out of bounds")
+        if index == 0:
+            return self.pop_front()
+        prev = self._node_at(index - 1)
+        node = prev.next
+        prev.next = node.next
+        node.next = None
+
+
+
+    def remove_at(self, index, val):
+
+    
+
+
+
 
 
 
